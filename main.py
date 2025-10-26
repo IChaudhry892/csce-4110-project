419442f1a004b3bd0fb756942922c2383b47f312
Leann Kahal <LeannKahal@my.unt.edu>
2025-10-26 17:26:54 -0500

    testing

---
+++ main.py
@@ -0,0 +1,12 @@
+# plain-texts
+# 1234567890 & 1234567899
+
+# two letter-plaintexts
+# abcde-fghij & abcdefghii
+
+# key for both encryption algorithm
+# 32568894
+
+# AES encryption/decryption
+
+# DES encryption/decryption
