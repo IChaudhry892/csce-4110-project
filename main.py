e76b786c6762615ae605fea7b4acc17b151d7a5b
Leann Kahal <LeannKahal@my.unt.edu>
2025-10-26 17:38:34 -0500

    venv for pycryptodome

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad, unpad
+
 # plain-texts
 # 1234567890 & 1234567899
+plaintext1 = b'1234567890'
+plaintext2 = b'1234567899'
 
 # two letter-plaintexts
 # abcde-fghij & abcdefghii
+letplaintext1 = b'abcde-fghij'
+letplaintext2 = b'abcedfghii'
 
 # key for both encryption algorithm
 # 32568894
+key = b'32568894'
 
 # AES encryption/decryption
 
